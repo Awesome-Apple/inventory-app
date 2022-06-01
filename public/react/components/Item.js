ea85ed211cd6964b60530930bcf154ad79676d7b
Jordan Hornback <jordanphornback@gmail.com>
2022-06-01 09:32:14 -0700

    fixed image sizes

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -4,7 +4,7 @@ export const Item = (props) => {
 
   return <>
     <h3>{props.item.title}</h3>
-    <img src={props.item.image} alt={props.item.name} maxwidth/>
+    <img src={props.item.image} alt={props.item.name} style={{ maxWidth: 200, maxHeight: 200}}/>
   </>
 } 
 	
