7cc04554570c5f0b7471bf286070a874ad9daed5
Anabel-Santillan <anabels.20022@gmail.com>
2022-06-02 11:52:18 -0400

    commiting incomplete code so I can pull Jordan's code

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import apiURL from '../api';
 
 export const Item = ({item, setSingleItem}) => {
+const [title, setTitle] = useState('');
+const [price, setPrice] = useState(0);
+const [description, setDescription] = useState('');
+const [category, setCategory] = useState('');
+const [image, setImage] = useState('');
 
   const fetchItem = async () => {
     const response = await fetch(`${apiURL}/items/${item.id}`);
@@ -9,9 +14,19 @@ export const Item = ({item, setSingleItem}) => {
     setSingleItem(itemData)
   }
 
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    set
+  }
+// title price desc category image
+
   return <>
     <h3 onClick={fetchItem} >{item.title}</h3>
     <img src={item.image} alt={item.name} style={{ maxWidth: 200, maxHeight: 200}}/>
+    <form onSubmit={handleSubmit}>
+      <h4> Add a new item to the inventory</h4>
+      <input type = "text" placeholder='Title' value={}></input>
+    </form>
   </>
 } 
 	
